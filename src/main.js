f48a1091a49543330d15032696815bdf617709a5
AlexVovan <alehnovich.vova@gmail.com>
2021-11-22 12:49:23 +0300

    add basket and its functionality

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import cart from './modules/cart'
+import getGoods from './modules/getGoods'
+import search from './modules/search'
+import { smoothScroll } from './modules/smoothScroll'
+
+
+cart()
+getGoods()
+search()
+smoothScroll()
