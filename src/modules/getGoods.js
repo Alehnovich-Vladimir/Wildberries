18fb87a969f9490d74f0a22af8235c2839c86521
AlexVovan <alehnovich.vova@gmail.com>
2022-02-14 08:56:49 +0100

    add fetch to firebase

--- src/modules/getGoods.js
+++ src/modules/getGoods.js
@@ -31,7 +31,7 @@ const getGoods = () => {
     }
 
     const getData = (value, category) => {
-        fetch('/db/db.json')
+        fetch('https://wildberries-a15fd-default-rtdb.firebaseio.com/db.json')
             .then((response) => response.json())
             .then((data) => {
                 const array = category ? data.filter((item) => item[category] === value) : data
